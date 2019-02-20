3f2d1343b11a2a5c613816ba5609014d21eef3ec
Abrehamgit <gezahegnabereham@gmail.com>
2019-02-20 09:41:04 -0800

    reset size

--- LoginFormUser.java
+++ LoginFormUser.java
@@ -6,7 +6,7 @@ class LoginFormUser {
 		LoginForm g = new LoginForm();
 
 		g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		g.setSize(300,200);
+		g.setSize(900,300);
 		g.setVisible(true);
 	}
 }
